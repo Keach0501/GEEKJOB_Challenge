0be6669148069af60c81806e29f4e93b37b408cf
LAPTOP-VUR2K1IK\keygr <stone.river0501@gmail.com>
2018-05-31 17:43:00 +0900

    クラスの継承の課題を提出します。

---
+++ CampChallenge_java/CampChallenge_java/013.クラス/Class_inheritance_exe.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classuse0;
+
+/**
+ *
+ * @author keygr
+ */
+public class Class_inheritance_exe {
+    
+    public static void main (String[] arg){
+        
+        class_inheritance CC = new class_inheritance ();
+        
+        System.out.println(CC.date);
+        System.out.println(CC.num);
+        
+    }
+    
+}
---
+++ CampChallenge_java/CampChallenge_java/013.クラス/class_inheritance.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classuse0;
+
+/**
+ *
+ * @author keygr
+ */
+public class class_inheritance extends Classcreate {
+    
+    public void class_inheritance (String date, int num){
+        
+        this.date = "";
+        this.num = 0;
+    }
+    
+}
