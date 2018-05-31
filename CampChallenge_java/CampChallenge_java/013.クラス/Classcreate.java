2d41a64bb86b295fb0760cb9f838f06642d33e0f
LAPTOP-VUR2K1IK\keygr <stone.river0501@gmail.com>
2018-05-31 10:49:04 +0900

    クラス利用の課題

---
+++ CampChallenge_java/CampChallenge_java/013.クラス/Classcreate.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classuse0;
+
+/**
+ *
+ * @author keygr
+ */
+public class Classcreate {
+
+    public String date = "";
+    public int num = 0;
+    
+    public void setClasscreate(String d, int n){
+
+        this.date = d;
+        this.num = n;
+    }
+
+}
---
+++ CampChallenge_java/CampChallenge_java/013.クラス/Classcreate2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classuse0;
+
+/**
+ *
+ * @author keygr
+ */
+public class Classcreate2 {
+    
+    public static void main(String[] args){
+    
+        Classcreate one = new Classcreate();
+        
+        one.setClasscreate("day", 1);
+        
+        System.out.println(one);
+    }
+    
+}
