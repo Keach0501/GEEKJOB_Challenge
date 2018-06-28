54ec7620bcd158ec6193e3661e4762290959c35d
LAPTOP-VUR2K1IK\keygr <stone.river0501@gmail.com>
2018-06-28 15:06:21 +0900

    標準クラスのバイト数と文字数の取得を提出します。

---
+++ CampChallenge_java/CampChallenge_java/015.標準クラス/batechara_get.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package staclass;
+
+/**
+ *
+ * @author keygr
+ */
+public class batechara_get {
+    public static void main(String[] args){
+        String word = "石川貴一";
+        System.out.println(word.getBytes().length);
+        System.out.println(word.length());
+    }
+}
