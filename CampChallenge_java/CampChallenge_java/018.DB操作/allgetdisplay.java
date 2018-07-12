e6b6b4b00898b1ecbb268fbe0320390b0d35465e
LAPTOP-VUR2K1IK\keygr <stone.river0501@gmail.com>
2018-07-12 11:29:04 +0900

    DB操作の全情報の取得と表示の課題を提出します。

---
+++ CampChallenge_java/CampChallenge_java/018.DB操作/allgetdisplay.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbcontrol;
+
+import java.sql.*;
+
+/**
+ *
+ * @author keygr
+ */
+public class allgetdisplay {
+    public static void main(String[] args){
+        Connection db_con = null;
+        PreparedStatement db_st = null;
+        try{
+            db_con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/challenge_db?characterEncoding=UTF-8&serverTimezone=JST","keach","keach0501");
+            db_st = db_con.prepareStatement(
+                "select * from profiles");
+            ResultSet rs = db_st.executeQuery();
+            while (rs.next()){
+                System.out.println(
+                    rs.getInt("profilesID") + "," 
+                    + rs.getString("name") + ","
+                    + rs.getString("tel") + ","
+                    + rs.getInt("age") + ","
+                    + rs.getDate("birthday"));
+            }
+        }catch(SQLException e_sql){
+            System.out.println(
+                "接続時にエラーが発生しました:" + e_sql.toString());
+        }catch(Exception e){
+            System.out.println(
+                "接続時にエラーが発生しました:" + e.toString());
+        }finally{
+            if(db_con != null){    
+                try{
+                    db_st.close();
+                    db_con.close();
+                }catch (SQLException e_con){
+                    System.out.println(e_con.getMessage());
+                }
+            }
+        }
+    }
+}
